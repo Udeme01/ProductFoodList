f84533749cf62420823c61e1af574ddf53a35168
Udeme01 <emmanueluddy2@gmail.com>
2024-10-18 14:55:02 +0100

    created Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <>
+      <header>
+        <h1 className="font-redHat700 text-4xl">Desserts</h1>
+      </header>
+    </>
+  );
+};
+
+export default Header;
